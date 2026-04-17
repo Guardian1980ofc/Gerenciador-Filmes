37620e10e85171a9d6dc6250ec8dee6b9e4c0e7c
Edoardo henrique <2006edoardo@gmail.com>
2026-04-17 01:49:42 -0300

    feat: implement Director and Actor models with 1:N and N:N relationships

--- movies/admin.py
+++ movies/admin.py
@@ -1,7 +1,18 @@
 from django.contrib import admin
-from .models import Movie
+from .models import Movie, Genre, Director, Actor
 
 @admin.register(Movie)
 class MovieAdmin(admin.ModelAdmin):
+    list_display = ("name", "genre", "director", "release_date", "score")
+
+    list_filter = ("genre", "score")
+
+    search_fields = ("name",)
+
     readonly_fields = ('created_at', 'updated_at')
+
+    filter_horizontal = ('actors',)
     
+admin.site.register(Genre)
+admin.site.register(Director)
+admin.site.register(Actor)
--- movies/models.py
+++ movies/models.py
@@ -16,11 +16,49 @@ class BaseModel(models.Model):
     class Meta:
         abstract = True
 
+class Genre(BaseModel):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Director(BaseModel):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+    
+class Actor(BaseModel):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
 class Movie(BaseModel):
     name = models.CharField(max_length=100)
     description = models.TextField(max_length=500)
     release_date = models.DateField("Release Date")
     score = models.IntegerField(choices=SCORE_CHOICES, default=3)
+    genre = models.ForeignKey(
+        Genre,
+        on_delete=models.PROTECT,
+        related_name="movies",
+        null=True,
+        blank=True)
+    
+    director = models.ForeignKey(
+        Director, 
+        on_delete=models.PROTECT, 
+        related_name="movies",
+        null=True, 
+        blank=True
+    )
+
+    actors = models.ManyToManyField(
+        Actor, 
+        related_name="movies", 
+        blank=True
+    )
 
     def __str__(self):
         return self.name
