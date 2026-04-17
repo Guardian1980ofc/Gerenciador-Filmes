6b5d9f79bd6464cccb7e22423a8ef5dfdf89b186
Edoardo henrique <2006edoardo@gmail.com>
2026-04-17 00:48:10 -0300

    feat: add Movie model with inheritance and admin configuration

--- movies/admin.py
+++ movies/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Movie
 
-# Register your models here.
+@admin.register(Movie)
+class MovieAdmin(admin.ModelAdmin):
+    readonly_fields = ('created_at', 'updated_at')
+    
--- movies/models.py
+++ movies/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+SCORE_CHOICES = [
+    (1, "1"),
+    (2, "2"),
+    (3, "3"),
+    (4, "4"),
+    (5, "5"),
+]
+
+
+class BaseModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+class Movie(BaseModel):
+    name = models.CharField(max_length=100)
+    description = models.TextField(max_length=500)
+    release_date = models.DateField("Release Date")
+    score = models.IntegerField(choices=SCORE_CHOICES, default=3)
+
+    def __str__(self):
+        return self.name
